c9ad8a5d321b418adc8300879b6715461fa0727e
russuiulia <russuiulia.md@gmail.com>
2022-02-05 22:57:22 +0200

    Add project files.

---
+++ WebApplication1/Pages/Form.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class FormModel : PageModel
+    {
+        private readonly ILogger<FormModel> _logger;
+
+        public FormModel(ILogger<FormModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
